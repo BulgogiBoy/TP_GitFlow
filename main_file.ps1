5a1b6f0009b002eb04160832ed150e1182d383d1
BulgogiBoy <baudouin.nguyen@hotmail.fr>
2023-02-06 13:52:51 +0100

    My third task V2

--- main_file.ps1
+++ main_file.ps1
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+Write-Host “Task1”
 Write-Host “Task2”
-=======
 Write-Host “Task3”
->>>>>>> c37de1d (My third task)
-=======
-Write-Host “Task3”
->>>>>>> c37de1d3adb7566aed3dc1d1b91133d08324199e
